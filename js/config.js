06d4787b93d38b398482e1912f7a6e95dfecb265
Matth76380 <matthieu.lustig@gmail>
2019-09-18 14:05:58 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-1_mvZCYabUs
+        userPoolClientId: '', // e.g. 3e2fug4fdqepvo2t5cd72hou87
+        region: '' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
